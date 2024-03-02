7fc08ea6b1080dddc970af634d8cc36598326d7e
armanali.9brainz <armanali.9brainz@gmail.com>
2024-03-02 15:59:03 +0530

    changes multer package

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://socialmediaapp-1upg.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
