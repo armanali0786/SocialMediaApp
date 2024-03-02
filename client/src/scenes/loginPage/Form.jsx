e0b2c6ef59807f11e10b00efeda1f1c0ba29bc12
armanali.9brainz <armanali.9brainz@gmail.com>
2024-03-02 16:21:59 +0530

    changes in Nabvar

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -84,7 +84,6 @@ const Form = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     });
-    console.log(loggedInResponse);
     const loggedIn = await loggedInResponse.json();
     onSubmitProps.resetForm();
     if (loggedIn) {
